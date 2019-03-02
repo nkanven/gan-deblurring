1a99fbdf243296b3ba46ff889fc9f61cf409f1fb
= <=>
2019-03-02 12:05:47 +0100

    Comment blurring function in utils

--- util/util.py
+++ util/util.py
@@ -26,7 +26,7 @@ def ResnetBlock(x, dim, ksize, scope='rb'):
         net = slim.conv2d(net, dim, [ksize, ksize], activation_fn=None, scope='conv2')
         return net + x
 
-def blurImage():
+"""def blurImage():
     #face = scipy.misc.face()
     list_dir = os.listdir(r'C:\Users\Nkondog\Desktop\DL_Training_Pix')
     dir_path = r"C:\Users\Nkondog\Desktop\DL_Training_Pix"
@@ -40,7 +40,7 @@ def blurImage():
         #input()
         #face = face.filter(ImageFilter.GaussianBlur(radius=6))
         face.save(im.split("\\")[-1])
-        """with open('datalist_gopro.txt', 'a') as t:
+        with open('datalist_gopro.txt', 'a') as t:
             ti = "GOIMG/sharp/"+im+" GOIMG/blur/"+im+"\n"
             t.write(str(ti))
     #face.show()
@@ -65,6 +65,6 @@ def blurImage():
     plt.subplots_adjust(wspace=0, hspace=0., top=0.99, bottom=0.01,
                         left=0.01, right=0.99)
 
-    plt.show()"""
+    plt.show()
 
-blurImage()
+blurImage()"""
